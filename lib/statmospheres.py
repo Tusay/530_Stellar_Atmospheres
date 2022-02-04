2f3af37c1dad2447fdb0b9dec9b4f077bd0a400c
Nicholas Tusay <tusay@psu.edu>
2022-02-04 14:47:34 -0500

    HW3 and changes to statmospheres.py function library. Also standard plot format file plt_format.mplstyle

--- lib/statmospheres.py
+++ lib/statmospheres.py
@@ -3,6 +3,7 @@
 
 from astropy import units as u
 from astropy import constants as const
+from scipy.special import wofz
 import numpy as np
 
 # Calculate the Blackbody Planck Function for one frequency (nu) and one temp (T). 
@@ -35,15 +36,35 @@ def Planck_from_wavenumbers(T,nu_array,A=1) -> (u.erg * u.cm**-2 * u.Hz**-1 * u.
 def my_integrator(x,y):
     dx = np.diff(x)
     ave_y = [(y[i]+y[i-1])/2 for i in range(1,len(y))]
-    total = np.dot(dx,ave_y)*const.c
+    total = np.dot(dx,ave_y)
     return total 
 
 # A wrapper for the integrator allowing the user to specify the function to obtain y values
 def integrator_wrapper(x_min,x_max,density,x_units,function,*args):
     x_min*=x_units
     x_max*=x_units
-    x = np.linspace(x_min,x_max,density)
+    points = density*(x_max-x_min)
+    x = np.logspace(x_min,x_max,points)
     x=x[1:]
     y = function(*args,x)
-    answer = my_integrator(x,y)
+    answer = my_integrator(x,y)*const.c
     return answer
+
+# A better integrator
+def better_integrator(x,y):
+    dx = np.diff(x)
+    ave_y = np.diff(y)/2+y[:-1]
+    total = np.dot(dx,ave_y)
+    return total 
+
+def better_integrator_wrapper(x_min,x_max,density,function,*args):
+    points = round(density*(x_max-x_min))
+    x = np.linspace(x_min,x_max,points)
+    y = function(x, *args)
+    return better_integrator(x,y)
+
+def Voigt(x, x0, y0, a, sigma, gamma):
+    return y0 + a * np.real(wofz((x - x0 + 1j*gamma)/sigma/np.sqrt(2))) / sigma /np.sqrt(2*np.pi)
+
+def ave(array):
+    return (array[1:]+array[:-1])/2
